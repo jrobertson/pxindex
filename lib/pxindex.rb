fdaeebf3dab1d12fcfa578e4ac546192cbc6ba62
Your Name <you@example.com>
2016-12-01 22:26:06 +0000

    bug fix: Nested items more than 2 levels deep can now be searched.

--- lib/pxindex.rb
+++ lib/pxindex.rb
@@ -13,25 +13,24 @@ class PxIndex
     s, _ = RXFHelper.read raw_s
 
     @px = PolyrexHeadings.new(s).to_polyrex   
-    @stack = [@px.records]
+    @rs = @px.records
 
     @s = ''
-    @result = nil
 
   end
 
   def q?(s)
 
-    if s.length > @s.length + 1 or s[0..-2] != @s then
+    if (s.length - @s.length).abs > 1 or s[0..-2] != @s then
 
-      @stack = [@px.records]
+      @rs = @px.records
       
       s2 = ''
       
       a = s.chars.map do |x|
 
         s2 += x
-        found = search_records(s2, @stack)
+        found = search_records(s2, @rs)
         
         break if not found
         found
@@ -44,7 +43,7 @@ class PxIndex
         
     i = s.length-1
 
-    records = search_records(s, @stack)
+    records = search_records(s, @rs)
 
     @s = s
     
@@ -57,38 +56,45 @@ class PxIndex
 
   private
 
-  def search_records(raw_s, stack=@stack)
-        
-    return @result if raw_s[-1] == ' '
+  def search_records(raw_s, rs=@rs)
+    
+    
+    if raw_s[-1] == ' ' then
+
+      child_records = rs.flat_map(&:records)
+      
+      if child_records.length > 0 then
+        @rs = child_records
+        return child_records
+      else
+        return nil
+      end
+    end
+
     keywords = raw_s.split(/ /)
     
-    s = if keywords.length > 1 then
-
-      r = stack.last.flat_map(&:records)
-      stack << r if r.any?
-      
-      keywords.last
-      
-    else
-      raw_s
+    s = keywords.length > 1 ? keywords.last : raw_s
+    
+    a = rs.select {|x| x.title[0..s.length-1] == s}    
+    
+    if s.length == 1 and a.any? and keywords.length < 2 then
+      a = a.first.records
     end
     
-    a = stack.last.select {|x| x.title[0..s.length-1] == s}
-    
-    if a.any? then
+    if a.any?  then
       
-      stack << a
-      #@result = a.map(&:title)
-      @result = a
+      @rs = a
       
     else
 
-      r = stack.last.flat_map(&:records)
+      return nil unless keywords.length > 1
+      
+      r = rs.flat_map(&:records)
 
       if r.any? then
         
-        stack << r                
-        search_records(s, stack)
+        @rs = r                
+        search_records(s, rs)
         
       else
         
