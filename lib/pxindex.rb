f9637f217ee68f4b94c2bb87a5e4d5a793ef4a35
James Robertson <digital.robertson@gmail.com>
2022-02-23 15:33:53 +0000

    improvement: Now using RXFReader as a replacement for RXFHelper.

--- lib/pxindex.rb
+++ lib/pxindex.rb
@@ -5,7 +5,7 @@
 require 'nokogiri'
 require 'pxindex-builder'
 require 'polyrex-headings'
-
+require 'rxfreader'
 
 
 class PxIndex
@@ -13,8 +13,8 @@ class PxIndex
   def initialize(raw_s=nil, debug: false, allsorted: false, indexsorted: false)
 
     @allsorted, @indexsorted = allsorted, indexsorted
-    @debug = debug    
-    
+    @debug = debug
+
     read raw_s if raw_s
 
   end
@@ -22,140 +22,140 @@ class PxIndex
   # Returns a PxIndexBuilder object which can build from am index or phrases
   #
   def import(s)
-    
+
     read(PxIndexBuilder.new(s, debug: @debug).to_s)
-    
+
   end
-  
+
   def parent()
     @rs.first
   end
 
   def q?(s)
-    
+
     return @a.last if s == @s
     puts '@s : ' + @s.inspect if @debug
     puts 's: ' + s.inspect if @debug
-    
-    # @s is used to store the previous string input to compare it with 
+
+    # @s is used to store the previous string input to compare it with
     # the new string input
-    
+
     if (s.length - @s.length).abs >= 1 or s[0..-2] != @s then
-      
-      @s = s      
+
+      @s = s
 
       @rs = @px.records.flat_map(&:records)
-      
+
       s2 = ''
-      
+
       @a = s.chars.map do |x|
 
         s2 += x
         found = search_records(s2, @rs)
-        
+
         break if not found
         found
-        
+
       end
 
       return @a ? @a.last  : nil
-      
+
     end
-  
+
     return []
-  
+
   end
 
   alias query q?
-  
+
   def build_html()
-    
+
     @px.each_recursive do |x, parent|
-      
+
       if x.is_a? Entry then
-        
+
         trail = parent.attributes[:trail]
         s = x.title.gsub(/ +/,'-')
         x.attributes[:trail] = trail.nil? ? s : trail + '/' + s
-        
+
       end
-      
+
     end
 
     doc  = Nokogiri::XML(@px.to_xml)
     xsl  = Nokogiri::XSLT(xslt())
 
     html_doc = Rexle.new(xsl.transform(doc).to_s)
-        
-    html_doc.root.css('.atopic').each do |e|      
-      
+
+    html_doc.root.css('.atopic').each do |e|
+
       puts 'e: ' + e.parent.parent.xml.inspect if @debug
-      
+
       href = e.attributes[:href]
       if href.empty? or href[0] == '!' then
-        
+
         if block_given? then
-          
+
           yield(e)
-          
+
         else
-          
+
           e.attributes[:href] = '#' + e.attributes[:trail].split('/')\
               .last.downcase
-          
+
         end
       end
     end
-    
+
     html_doc.xml(pretty: true, declaration: false)
-    
+
   end
-  
+
   def to_px()
     @px
   end
-  
+
   def to_s()
     @raw_px
   end
 
   private
-  
+
   def read(raw_s)
-    
-    s, _ = RXFHelper.read raw_s
-    
+
+    s, _ = RXFReader.read raw_s
+
     lines = s.lines
 
     header = []
     header << lines.shift until lines[0].strip.empty?
-    
+
     a = LineTree.new(lines.join.gsub(/^# [a-z]\n/,'')).to_a
     a2 = a.group_by {|x| x.first[0] }.sort.to_a
-    
-    s2 =  a2.map do |x|      
+
+    s2 =  a2.map do |x|
       '# ' + x[0] + "\n\n" + \
           treeize(@allsorted || @indexsorted ? sort(x[-1]) : x[-1])
     end.join("\n\n")
-    
+
     puts 's2: ' + s2.inspect if @debug
     @raw_px = header.join + "\n" + s2
 
-    @px = PolyrexHeadings.new(@raw_px).to_polyrex   
+    @px = PolyrexHeadings.new(@raw_px).to_polyrex
     @rs = @px.records.flat_map()
 
     @s = ''
-    @a = []    
+    @a = []
   end
 
   def search_records(raw_s, rs=@rs)
-    
+
     puts 'raw_s : ' + raw_s.inspect if @debug
-    
+
     if raw_s[-1] == ' ' then
 
       child_records = rs.flat_map(&:records)
-      
+
       if child_records.length > 0 then
         @rs = child_records
         return child_records
@@ -165,67 +165,67 @@ class PxIndex
     end
 
     keywords = raw_s.split(/ /)
-    
+
     s = keywords.length > 1 ? keywords.last : raw_s
-    
-    a = rs.select {|x| x.title[0..s.length-1] == s}    
-    
+
+    a = rs.select {|x| x.title[0..s.length-1] == s}
+
     if @debug then
-      puts 'a: ' + a.inspect 
+      puts 'a: ' + a.inspect
       if a.any? then
         puts 'a.map ' + a.map(&:title).inspect
       end
-    end    
-    
+    end
+
     if a.any?  then
-      
+
       @rs = a
-      
+
     else
 
       return nil unless keywords.length > 1
-      
+
       r = rs
 
       if r.any? then
-        
-        @rs = r                
+
+        @rs = r
         search_records(s, rs)
-        
+
       else
-        
+
         return nil
-        
+
       end
-      
+
     end
 
   end
-  
+
   def sort(a)
-    
+
     puts 'sorting ... a: ' + a.inspect if @debug
     return sort_children(a) if a.first.is_a? String
-    
-    r = a.sort_by do |x| 
+
+    r = a.sort_by do |x|
       next unless x[0].is_a? String
       x[0]
     end
-    
+
     puts 'after sort: ' + r.inspect if @debug
-    
+
     r
-    
+
   end
-  
+
   def sort_children(a)
     [a[0]] + a[1..-1].sort_by {|x| x[0]}
   end
-  
+
   def treeize(obj, indent=-2)
 
     if obj.is_a? Array then
-      
+
       r = (@allsorted ? sort(obj) : obj).map {|x| treeize(x, indent+1)}.join("\n")
       puts 'r: ' + r.inspect if @debug
       r
@@ -237,7 +237,7 @@ class PxIndex
     end
   end
 
-  
+
   def xslt()
 <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
@@ -283,8 +283,8 @@ class PxIndex
 </xsl:template>
 
 
-</xsl:stylesheet>    
+</xsl:stylesheet>
 EOF
-  end  
+  end
 
 end
--- pxindex.gemspec
+++ pxindex.gemspec
@@ -1,16 +1,16 @@
 Gem::Specification.new do |s|
   s.name = 'pxindex'
-  s.version = '0.2.2'
+  s.version = '0.3.0'
   s.summary = 'Experimental gem to facilitate a keyword search, ' + 
       'by querying a Polyrex document representing a hierarchical lookup table'
   s.authors = ['James Robertson']
   s.files = Dir['lib/pxindex.rb']
-  s.add_runtime_dependency('nokogiri', '~> 1.10', '>=1.10.1s')
-  s.add_runtime_dependency('pxindex-builder', '~> 0.2', '>=0.2.0')
-  s.add_runtime_dependency('polyrex-headings', '~> 0.1', '>=0.1.9')
+  s.add_runtime_dependency('nokogiri', '~> 1.13', '>=1.13.3')
+  s.add_runtime_dependency('pxindex-builder', '~> 0.2', '>=0.2.1')
+  s.add_runtime_dependency('polyrex-headings', '~> 0.3', '>=0.3.0')
   s.signing_key = '../privatekeys/pxindex.pem'
   s.cert_chain  = ['gem-public_cert.pem']
   s.license = 'MIT'
-  s.email = 'james@jamesrobertson.eu'
+  s.email = 'digital.robertson@gmail.com'
   s.homepage = 'https://github.com/jrobertson/pxindex'
 end
